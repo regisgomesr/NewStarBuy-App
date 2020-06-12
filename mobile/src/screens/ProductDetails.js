0c4f17b6f43923857bd15dd3f5561938a104270e
Regis Ribeiro <regisgomesr@gmail.com>
2020-06-12 20:55:45 -0300

    Add Navigation between ProductsList and ProductDetails

--- mobile/src/screens/ProductDetails.js
+++ mobile/src/screens/ProductDetails.js
@@ -2,17 +2,10 @@ import React from 'react'
 import { View, StyleSheet, Text, Button } from 'react-native'
 
 
-export function ProductsList({ navigation }) {
+export function ProductDetails() {
   return (
     <View style={styles.container}>
-      <Text>Bem-vindo ao NewStarBuy App</Text>
-      
-      <Button 
-        title={'Abrir detalhe do produto'}
-        onPress={() => {
-          navigation.navigate('ProductDetails');
-        }}
-      />
+      <Text>Detalhe do Produto.</Text>
     </View>
   )
 }
